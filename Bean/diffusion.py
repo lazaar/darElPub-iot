dcfe99977eee9ba1faa9cfd07566e78093b2e43d
ilazaar <ilazaar@sqli.com>
2016-04-06 18:41:56 +0100

    Diffusion Bean

--- Bean/diffusion.py
+++ Bean/diffusion.py
@@ -4,3 +4,19 @@ class diffusion:
         self.number =number;
         self.date =date;
         self.id_video =id_video;
+    def getId(self):
+        return self.id;
+    def setId(self,id):
+        self.id=id
+    def getNumber(self):
+        return self.number
+    def setNumber(self, number):
+        self.number = number
+    def getDate(self):
+        return self.date
+    def setDate(self,date):
+        self.date = date
+    def getIdVideo(self):
+        return self.id_video
+    def setIdVideo(self,id_video):
+        self.id_video = id_video
