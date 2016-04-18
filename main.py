6693641cd8c18f99ac3e8d8665f1fc3d18258bae
ilazaar <ilazaar@sqli.com>
2016-04-18 18:36:24 +0100

    Download Videos

---
+++ main.py
@@ -0,0 +1,38 @@
+import os, urllib2, mainDAO, json
+from datetime import datetime
+
+# Ce script doit etre execute a chaque demarage
+isFirstTime = not os.path.isfile('darelpub.db')
+dao = mainDAO.mainDAO()
+currentDate = datetime.now();
+
+def sendData():
+	print("Send Data")
+	# Reinitialisation de la base
+
+def getNewData():
+	print("get New Data")
+	# Reinitialisation de la base
+	url = "http://10.42.6.190/siblu/videos.json"
+	data = urllib2.urlopen(url)
+	videos = json.loads(data.read())
+	for video in videos['videos']: 
+		dao.insertVideo((video['name'], video['qte'], 1))
+		if(not os.path.isfile('videos/'+video['name']+'.mp4')):
+			os.system("python youtube-dl --output ./videos/%s.mp4 %s"%(video['name'],'https://www.youtube.com/watch?v='+video['name']))
+
+# mettre HDMI du raspberry en mode off
+
+# verifier est ce qu'il s'agit du premier demarrage du raspberry
+if(isFirstTime):
+	print('First Time')
+	dao.initDB()
+	dao.setData('lastDate', currentDate.strftime("%d/%m/%Y"))
+else:
+	# verifier est ce qu'il s'agit du premier demarrage du raspberry dans la journee
+	lastDate = datetime.strptime(dao.getData('lastDate'), "%d/%m/%Y")
+	if(lastDate < currentDate):
+		# Envoi des donnees de la journee precedentes
+		sendData()
+		# L'Appel du WS pour recuperation des donnees
+		getNewData()
--- mainDAO.py
+++ mainDAO.py
@@ -1,18 +1,50 @@
 import sqlite3
-import Bean.video
 
 class mainDAO:
-
     def __init__(self):
         self.conn = sqlite3.connect('darelpub.db')
 
     def initDB(self):
         try:
             cursor = self.conn.cursor()
-            for line in open('../script.sql'):
+            for line in open('./script.sql'):
                 cursor.execute(line)
             self.conn.commit()
         except Exception as e:
-            print("Erreur d'execution du script")
-            conn.rollback()
-db = video(1,'test','','')
+            print("Erreur d'execution du script", e)
+            self.conn.rollback()
+    def executeQuery(self, query):
+        try:
+            cursor = self.conn.cursor()
+            cursor.execute(query)
+            self.conn.commit()
+            return cursor
+        except Exception as e:
+            print("Erreur d'execution du query :", query, e)
+            self.conn.rollback()
+
+    def insertVideo(self, videos):
+        try:
+            cursor = self.conn.cursor()
+            cursor.execute('INSERT INTO video (name, qte, show) VALUES (?,?,?)', videos)
+            self.conn.commit()
+        except Exception as e:
+            print("Erreur d'execution du query", e)
+            self.conn.rollback()
+    def getShowVideos(self):
+        c = self.executeQuery('SELECT * FROM video WHERE show = 1')
+        rows =  c.fetchall()
+        return rows
+    def insertDiffusion(self,diffusion ):
+        try:
+            cursor = self.conn.cursor()
+            cursor.execute('INSERT INTO diffusion VALUES (?,?,?)', diffusion)
+            self.conn.commit()
+        except Exception as e:
+            print("Erreur d'execution du query", e)
+            self.conn.rollback()
+    def getData(self,cle):
+        c = self.executeQuery("SELECT valeur FROM data WHERE cle = '"+cle+"'")
+        return c.fetchall()[0][0]
+    def setData(self, cle, valeur):
+        c = self.executeQuery("UPDATE data SET valeur='"+valeur+"' WHERE cle='"+cle+"'")
